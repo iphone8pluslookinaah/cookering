a958b6a34cc25f6c58013d548b3057d53db6d38b
Kai <kaiweng.23@ichat.sp.edu.sg>
2024-05-22 18:15:37 +0800

    first round

---
+++ datatypes.py
@@ -0,0 +1,18 @@
+x = "words"
+print(x, type(x))
+x = 20
+print(x, type(x))
+x = 20.5
+print(x, type(x))
+x = ["buh", 29, "in sq bracket"]
+print(x, type(x))
+x = ("buh", 29, "in bracket")
+print(x, type(x))
+x = range(6)
+print(x, type(x))
+x = {"ip" : "229.246.19.145", "subnet mask" : "255.255.255.0"}
+print(x, type(x))
+x = True
+print(x, type(x))
+x = None
+print(x, type(x))
---
+++ sort.py
@@ -0,0 +1,15 @@
+strings = ["banana", "apple", "cherry"]
+sorted_strings = sorted(strings)
+print(sorted_strings)  # Output: ['apple', 'banana', 'cherry']
+
+strings = ["banana", "apple", "cherry"]
+strings.sort()
+print(strings)  # Output: ['apple', 'banana', 'cherry']
+
+strings = ["banana", "apple", "cherry"]
+sorted_strings = sorted(strings, reverse=True)
+print(sorted_strings)  # Output: ['cherry', 'banana', 'apple']
+
+strings = ["banana", "apple", "cherry"]
+strings.sort(reverse=True)
+print(strings)  # Output: ['cherry', 'banana', 'apple']
